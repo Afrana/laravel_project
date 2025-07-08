dea2ffd09a7be95e36a059ffc00dd854fc56c8e7
Afrana <afrananasar95@gmail.com>
2025-07-08 18:18:01 +0530

    check review

--- run_review.php
+++ run_review.php
@@ -8,6 +8,29 @@ use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
 use PhpParser\Node;
 
+class VarDumpVisitor extends NodeVisitorAbstract {
+    private $file;
+    private $warnings = [];
+
+    public function __construct($file) {
+        $this->file = $file;
+    }
+
+    public function enterNode(Node $node) {
+        if ($node instanceof Node\Expr\FuncCall &&
+            $node->name instanceof Node\Name &&
+            $node->name->toString() === 'var_dump') {
+
+            $line = $node->getLine();
+            $this->warnings[] = "Warning: 'var_dump()' found on `line {$line}` ";
+        }
+    }
+
+    public function getWarnings() {
+        return $this->warnings;
+    }
+}
+
 $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
 $changedFiles = file('changed_files.txt', FILE_IGNORE_NEW_LINES);
 
@@ -16,42 +39,16 @@ $issues = [];
 foreach ($changedFiles as $file) {
     if (!str_ends_with($file, '.php')) continue;
 
-    $issues[] = "file: `$file`";
+    $issues[] = "`file: $file`";
 
     $code = file_get_contents($file);
     $ast = $parser->parse($code);
 
-    // Custom Visitor class to collect issues
-    class VarDumpVisitor extends NodeVisitorAbstract {
-        private $file;
-        private $warnings = [];
-
-        public function __construct($file) {
-            $this->file = $file;
-        }
-
-        public function enterNode(Node $node) {
-            if ($node instanceof Node\Expr\FuncCall &&
-                $node->name instanceof Node\Name &&
-                $node->name->toString() === 'var_dump') {
-
-                $line = $node->getLine();
-                $this->warnings[] = "Warning: 'var_dump()' found in on `line {$line}`";
-            }
-        }
-
-        public function getWarnings() {
-            return $this->warnings;
-        }
-    }
-
-    // Traverse with visitor
     $traverser = new NodeTraverser();
     $visitor = new VarDumpVisitor($file);
     $traverser->addVisitor($visitor);
     $traverser->traverse($ast);
 
-    // Collect issues
     $fileWarnings = $visitor->getWarnings();
     $issues = array_merge($issues, $fileWarnings);
 }
