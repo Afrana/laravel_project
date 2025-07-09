ee5ee0432c1eeabd245e62fdc561584de9c936b0
Afrana <afrananasar95@gmail.com>
2025-07-09 07:08:41 +0530

    check review

--- app/Http/Requests/StorePageRequest.php
+++ app/Http/Requests/StorePageRequest.php
@@ -80,5 +80,6 @@ class StorePageRequest extends FormRequest
             'active' => __('messages.activity'),
             'icon' => __('messages.icon'),
         ], $multilingualAttributes);
+        var_dump($multilingualAttributes);
     }
 }
